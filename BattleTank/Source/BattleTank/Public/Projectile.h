e4c98fb2e526b57c7aa4cf7b2ee3bc14f617ad94
samlaw2112 <samuel.lawrence2112@gmail.com>
2019-05-15 15:07:54 +0100

    Revert "Projectile movement component not compiling"
    This reverts commit 24419ba9854adc060571bd6f894a82c70238f818.

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "GameFramework/ProjectileMovementComponent"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -23,8 +22,5 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-private:
-	UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
 
 };
